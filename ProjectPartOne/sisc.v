14fd914767f716e6f73db455a42b435ee201b5ea
Cole Pierce <31912242+colemtg@users.noreply.github.com>
2018-02-18 18:10:33 -0600

    Close to working,  not loading regs correctly atm

--- ProjectPartOne/sisc.v
+++ ProjectPartOne/sisc.v
@@ -27,14 +27,17 @@ module sisc (clk, rst_f, ir);
   rf register(clk, ir[19:16], mux4ToRF, ir[23:20], mux32ToRF, ctrlToRFrf_we, rfToALUrsa, rfToALUrsb);
   alu logicUnit(clk, rfToALUrsa, rfToALUrsb, ir[15:0], ctrlToALUalu_op, aluToMUX32, aluToSTATstat, aluToSTATenable);
   mux32 muxALU(aluToMUX32, 1'b0, ctrlToMUX32sel, mux32ToRF);   
-  statref status(clk, aluToSTATstat, aluToSTATenable, statToCTRLstat);
+  statreg status(clk, aluToSTATstat, aluToSTATenable, statToCTRLstat);
   ctrl control(clk, rst_f, ir[31:28], ir[27:24], statToCTRLstat, ctrlToRFrf_we, ctrlToALUalu_op, ctrlToMUX32sel);
 
 
 
   initial
     begin
-
+      //monitor IR, R1-R6,RD_SEL,ALU_OP,WB_SEL,RF_WE, rf write_data
+      $monitor("Time=%h, IR=%h,R1=%h, R2=%h, R3=%h, R4=%h, R5=%h, R6=%h, RD_SEL=%h, ALU_OP=%h, WB_SEL=%h, RF_WE=%h, WRITE_DATA=%h"
+      ,$time,ir,register.ram_array[1],register.ram_array[2],register.ram_array[3],register.ram_array[4],register.ram_array[5],
+      register.ram_array[6], 1'b0, ctrlToALUalu_op, ctrlToMUX32sel, ctrlToRFrf_we, mux32ToRF);
     end
   
 // put a $monitor statement here.  
